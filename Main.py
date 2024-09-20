6494e07565b6ba0e4cad78f41093c58fbbf98e0c
Evan <estice@ufl.edu>
2024-09-20 14:27:36 -0400

    FEAT: Added correct typemask values for T-Mobile

--- Main.py
+++ Main.py
@@ -1,10 +1,24 @@
 class Typemask:
-    def __init__(self):
+    # carriers: [1 - T-Mobile]
+    def __init__(self, carrier):
         self.typemask = 0
         self.vvmEnabled = False
         self.dataEnabled = False
         self.mmsEnabled = False
 
+        self.vvmIncrement = 0
+        self.dataIncrement = 0
+        self.mmsIncrement = 0
+
+        if carrier == 1:
+            self.vvmIncrement = 2
+            self.dataIncrement = 1
+            self.mmsIncrement = 4
+
     def getTypemask(self):
-        if self.vvmEnabled == True:
-            self.typemask += 0 # need to add real values
+        if self.vvmEnabled:
+            self.typemask += self.vvmIncrement
+        if self.dataEnabled:
+            self.typemask += self.dataIncrement
+        if self.mmsEnabled:
+            self.typemask += self.mmsIncrement
