bc605b70786ed8940580542612e808bc3bf83f81
Evan <estice@ufl.edu>
2024-09-21 15:24:38 -0400

    FEAT: Initial HTML/CSS styling

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from typemask import Typemask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def index() -> object:
-    return 'TESTING out Flask'
+    return render_template('mainpage.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000, debug=True)
